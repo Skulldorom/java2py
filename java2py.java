e9d623c760ae9d56ff310bb065c972d5e0deccd5
Skulldorom <51134009+Skulldorom@users.noreply.github.com>
2020-01-16 14:18:26 +0300

    Create java2py.java

---
+++ java2py.java
@@ -0,0 +1,12 @@
+package javapy;
+
+public class java2py{
+
+    public static void print(final String string) {
+    System.out.println(string);
+  }
+
+  public static String input() {
+      return System.console().readLine();
+    }
+}
